5a244ad46e9ae6729399b70df1428bc6df6f401b
Mary-melvin <162853197+Mary-melvin@users.noreply.github.com>
2024-06-11 23:21:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+console.log("\n \t Welcom to Mathias - Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter you guess number(Number limit from 1 to 5):",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congartulations ! You Guessed a Correct Number.");
+}
+else {
+    console.log("Sorry, You Guessed a Wrong Number.");
+}
